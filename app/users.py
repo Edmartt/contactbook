b446249656a9008a34f6352b13555b88e76e3c51
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-08-11 01:21:21 -0500

    Now password is a property and it's setter use generate_password_hash function for encryption. The querys in the execute statements is a variable now and for exceptions is used the logging module.

--- app/users.py
+++ app/users.py
@@ -1,3 +1,5 @@
+import logging
+from werkzeug.security import generate_password_hash, check_password_hash
 from .db import get_db
 
 
@@ -8,53 +10,62 @@ class User:
         self.email = email
         self.password = password
 
+    @property
+    def password(self):
+        raise AttributeError('This property can\'t be read')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
     def log_user(self):
-        cursor = get_db().cursor()
+
+        connection = get_db()
+        cursor = connection.cursor()
+        
+        query = 'SELECT * FROM user WHERE username=?'
+        
         try:
-            cursor.execute(
-                'SELECT * FROM user WHERE username=?',
-                (self.username,))
+            cursor.execute(query, (self.username,))
             user = cursor.fetchone()
             if user:
                 return user
         except Exception as ex:
-            print(ex)
+            logging.exception('Error in log_user: ')
 
     def reg_user(self):
-        cursor = get_db().cursor()
-        conection = get_db()
+        connection = get_db()
+        cursor = connection.cursor()
+        
+        query = 'INSERT INTO user(username, email, password) VALUES(?, ?, ?)'
 
         try:
-            cursor.execute(
-                'INSERT INTO user(username, email, password) VALUES(?, ?, ?)',
-                (self.username, self.email, self.password))
-
-            conection.commit()
+            cursor.execute(query, (self.username, self.email,
+                                   self.password_hash))
+            connection.commit()
         except Exception as ex:
-            print(ex)
+            logging.exception('Error in reg user: ')
 
     def check_user_by_username(self, username):
-
         cursor = get_db().cursor()
-
+        query = 'SELECT id FROM user WHERE username=?'
         try:
-            result = cursor.execute('SELECT id FROM user WHERE username=?',
-                                    (username,)).fetchone()
+            cursor.execute(query, (username,))
+            result = cursor.fetchone()
             if result:
                 return True
 
         except Exception as ex:
-            print(ex)
+            logging.exception('Error in check_user_by_username: ')
 
     @staticmethod
     def check_user_by_id(id):
-
         cursor = get_db().cursor()
-
+        query = 'SELECT * FROM user WHERE id=?'
         try:
-            user = cursor.execute(
-                'SELECT * FROM user WHERE id=?', (id,)).fetchone()
+            cursor.execute(query , (id,)).fetchone()
+            user = cursor.fetchone()
             if user:
                 return user
         except Exception as ex:
-            print(ex)
+            logging.exception('Error in check_user_by_id')
