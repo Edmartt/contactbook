002a554fb5f36949b593a0026053479f127e3ddf
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-08-14 23:23:25 -0500

    Added config file for changing settings of the app depending on the environment that you need

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev'
+    DEBUG = True
+    DATABASE = os.path.join(basedir, 'data-dev.sqlite')
+
+class TestingConfig(Config):
+    TESTING = True
+    DATABASE = 'sqlite://'
+
+class ProductionConfig(Config):
+    os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+        'development': DevelopmentConfig,
+        'testing': TestingConfig,
+        'production': ProductionConfig,
+        'default': DevelopmentConfig
+        }
